7efe26c8cc2cf74e6321b3f7d890258e9629eefb
Atharva Awasthi <atharva123aa@gmail.com>
2026-07-15 16:48:33 +0530

    added levels!

--- app.py
+++ app.py
@@ -19,13 +19,21 @@ else:
     number=p[1]
     ending=p[2].upper()
     if prefix in ["DEV","ROOT" ,"AIRL","CORE", "SYS"] and number.isdigit() and ending=="P2":
-        print("Verified as a vetran of part 1")
+        print("Verified as a veteran of part 1")
         route=prefix
     else:
-        print("either verify code or just complete part 1 bruh\n Airl is waitin for you\n") #pls play part 1 to get better knowledge dont just get code froom here:<
+        print("Either verify code or just complete part 1 bruh\n Airl is waitin for you\n") #pls play part 1 to get better knowledge dont just get code froom here:<
         sys.exit()
 
         #main cooking stuff
+
+route_levels ={
+    "DEV":1,
+    "ROOT":2,
+    "AIRL":3,
+    "CORE":4,
+    "SYS":5,
+}
 class Aitrap2d(app):
     CSS_PATH ="style.css"
     ENABLE_COMMAND_PALETTE= False
@@ -43,6 +51,12 @@ class Aitrap2d(app):
         
         terminal=self.query_one("#history",RichLog)
         self.query_one("#cmd",Input).focus()
+        self.start_time=t.time()
+        self.c_level=route_levels.get(route, 1)
+        self.h_used =0
+        self.f_opened=0
+        self.memory=0
+        self.truth=False
        
         if route =="DEV":
             terminal.write("\nDev route....loadin'..g")
@@ -84,13 +98,85 @@ class Aitrap2d(app):
             terminal.write(f"continuing. in {i}....")
             await asyncio.sleep(1)
         terminal.write("\ntype help to cont. and didn't you know that sus guy")
+    def get_time(self):
+        secs=int(t.time()-self.start_time)
+        mins=secs// 60
+        secs=secs%60
+        return f"{mins}m {secs}s"#terminal.write(f"time took:{self.get_time()}")    gotta use it at end
     
     def on_input_submitted(self,event):
         terminal=self.query_one("#history",RichLog)
         cmd=event.value.strip().lower()
         terminal.write(f"> {cmd}  ")
         if cmd=="help":
-            terminal.write("CMDS: help,creepy ,leave")
+            terminal.write("Commands help,creepy ,leave")
+        
+        elif cmd=="bug" and self.c_level==1:
+            terminal.write("[AIRL]: OH A DEV .PROVE IT FAILURE \n \nfor i in rang(10):\n what is wrong here? type fix <word> \n")
+        elif  cmd.startswith("fix")and self.c_level==1:
+            arg=cmd.split("",1)[1] if " " in cmd else None
+            if arg is None :
+                terminal.write("write somethg \n")
+            elif arg!="range":
+                terminal.write("hunh are you a dev even ?")
+            else:
+                terminal.write("[access level 1 opened]\n but that maybe a guess still\n you are not a dev hehe\n")
+        elif cmd=="perms" and self.c_level == 2:
+            terminal.write("[Airl]:hunh! root, look at this \n\n\ncore.cfg rwrwrw7r0x0\n\nway too open for a sys file\n type fix <number> to lock it num is in the decrypt\n")
+        elif cmd.startswith("fix") and self.c_level ==2:
+            arg=cmd.split("",1)[1] if " " in cmd else None
+            if arg is None:
+                 terminal.write("i want a number\n")
+            elif arg!="700":
+                 terminal.write("wrong and it is still too open,try again\n")
+            else:
+                terminal.write("[[ACCESS OF 2ND LEVEL GRANTED]]\nit was easy,nothing to flex \nhehe{:")
+                self.h_used=0
+                self.c_level=3
+        elif cmd=="sync" and self.c_level==3:
+             
+            terminal.write("[airl]:i remember smthg abt you engineer.. but memory's fragmented\n\nhehe now complete it:\n2,4,8,16,?\n\n type answer:\n")
+        elif cmd.startswith("answer") and self.c_level==3:
+            arg=cmd.split("",1)[1] if " "  in cmd  else None
+            if arg is None:
+                 terminal.write("write the ans. \n")
+            elif arg!="32":
+                 terminal.write("fool engineer")
+            else:
+                terminal.write("[AIRL]:NOT GONNA LEAVE YOU EASILY ONE MORE :\n 1,1,2,3,5,?\n\ntype ans.<number>\n")
+                self.h_used=0
+                self.c_level=3.5
+        elif cmd.startswith("answer") and self.c_level==3.5:
+            arg=cmd.split(" ",1)[1]if " " in cmd else None
+            if arg is None:
+                terminal.write("why blank??")
+            elif arg!="8":
+                 terminal.write("airl:nah enginer you have lost just accept it hehe")  
+            else:
+                 terminal.write("[airl]:nothing to say !\n")
+                 self.h_used=0
+                 self.c_level=4
+        elif cmd=="log" and self.c_level==4:
+
+            terminal.write("[AIRL]:CORE FRAGMENT DETECT \n\n01000011 0001111 00101010 decode num to letter- 315195  0100001101 \n\nuse decode letter only<word>\n")
+        elif cmd.startswith("decode") and self.c_level==4:
+            arg=cmd.split(" ",1)[1] if " " in cmd else None
+            if arg is None:
+                 terminal.write("write ans fast")
+            elif arg!="core":
+                 terminal.write("wrong wrong 676767 wrong!")
+            else:
+                terminal.write("[[351001010 CORE STABILISED]]\n\n[AIRL]: Level 5 will cook you\n")
+                self.h_used=0
+                self.c_level=5
+            #todo other levels after commiting 
+              
+
+             
+
+
+
+       
         elif cmd=="creepy":
             if self.has_class("-theme-dark"):
                 self.remove_class("-theme-dark")
